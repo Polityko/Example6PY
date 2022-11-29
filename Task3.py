83f655a7bc9c397369797455171d9edbe3c30599
Olga <politykoolga@outlook.com>
2022-11-29 13:55:19 +0300

    Task 3

---
+++ Task3.py
@@ -0,0 +1,10 @@
+# Задача 3. Найдите все простые несократимые дроби, лежащие между 0 и 1, знаменатель которых не превышает 11.
+
+import math
+res = []
+for i in range(2, 12):
+    for j in range(1, i):
+        if math.gcd(i, j) == 1:
+            res.append(f'{j}/{i}')
+print(res)
+
